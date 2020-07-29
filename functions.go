3340fbbe63e141f9d48c4a50c1004413497707ee
T.A.C.T.I.C.A.L <louis.dalibard@gmail.com>
2020-07-29 23:22:07 +0200

    Add comments

--- functions.go
+++ functions.go
@@ -16,18 +16,25 @@ import (
         "path/filepath"
 )
 
+// Check for errors.
 func check(e error) {
         if e != nil {
-                log.Fatal(e)
-                panic(e)
+                log.Fatal(e);
+                panic(e);
         }
 }
 
+// Handle encrypting files.
 func encryptfile(filenamepointer *string, s string, progressbar *gtk.ProgressBar) {
+        // Check if user pressed cancel button (prevents segmentation fault).
         if filenamepointer == nil {
                 return
         }
+
+        // Dereference filename pointer.
         filename := *filenamepointer
+
+        // Make a temporary zip file.
         fi, err := os.Stat(filename)
         var cmd *exec.Cmd
         check(err)
@@ -38,44 +45,73 @@ func encryptfile(filenamepointer *string, s string, progressbar *gtk.ProgressBar
                 cmd = exec.Command("zip", "-j", "nekotemp.zip", filename)
         }
         cmd.Run()
+
+        // Update progress bar.
         progressbar.SetFraction(0.5)
+
+        // Read temporary zip file bytes.
         dat, _ := ioutil.ReadFile("nekotemp.zip")
 
+        // Create key hash from password.
         h := sha256.New()
         h.Write([]byte(s))
+
+        // Convert key to string.
         key := hex.EncodeToString(h.Sum(nil))
 
+        // Encrypt data.
         encrypted := encrypt(dat, key)
 
+        // Write output data to file.
         f, err := os.OpenFile(filepath.Dir(filename)+"/output.neko", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
         check(err)
         defer f.Close()
         f.WriteString(encrypted)
+
+        // Delete temporary zip file.
         os.Remove("nekotemp.zip")
 }
 
+// Handle decrypting files.
 func decryptfile(filenamepointer *string, s string, progressbar *gtk.ProgressBar) {
+        // Check if user pressed cancel button (prevents segmentation fault).
         if filenamepointer == nil {
                 return
         }
+        // Dereference filename pointer.
         filename := *filenamepointer
+
+        // Update progress bar.
         progressbar.SetFraction(0.5)
+
+        // Read input file bytes.
         dat, _ := ioutil.ReadFile(filename)
 
+        // Create key hash from password.
         h := sha256.New()
         h.Write([]byte(s))
+
+        // Convert key to string.
         key := hex.EncodeToString(h.Sum(nil))
 
+        // Decrypt data.
         decrypted := decrypt(string(dat), key)
+
+        // Write data to temporary zip file.
         f, err := os.OpenFile("nekotemp.zip", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
         check(err)
         defer f.Close()
         f.WriteString(decrypted)
+
+        // Unzip temporary zip file into directory of input file.
         cmd := exec.Command("unzip", "nekotemp.zip", "-d", filepath.Dir(filename))
         cmd.Run()
+
+        // Delete temporary zip file.
         os.Remove("nekotemp.zip")
 }
 
+// Handle encrypting data.
 func encrypt(plaintext []byte, keyString string) (encryptedString string) {
 
         //Since the key is in string, we need to convert decode it to bytes
@@ -102,6 +138,7 @@ func encrypt(plaintext []byte, keyString string) (encryptedString string) {
         return fmt.Sprintf("%x", ciphertext)
 }
 
+// Handle decrypting data.
 func decrypt(encryptedString string, keyString string) (decryptedString string) {
 
         key, _ := hex.DecodeString(keyString)
